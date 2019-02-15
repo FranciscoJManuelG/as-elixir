6a011a7d755dbcce38ef82512d16c85d2939593c
Francisco Javier Manuel García <francisco.j.manuelg@udc.es>
2019-02-15 09:10:40 +0100

    Ejercicio 1

---
+++ e1_pattern_matching/lib/boolean.ex
@@ -0,0 +1,12 @@
+defmodule Boolean do
+	def b_not(true),do: false
+	def b_not(false),do: true
+
+
+	def b_and(true, true),do: true
+	def b_and(_,_),do: false
+
+	
+	def b_or(false, false),do: false
+	def b_or(_, _),do: true
+end
