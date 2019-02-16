ea379116334710ccc03eed8c90a649674da8e926
Francisco Javier Manuel García <francisco.j.manuelg@udc.es>
2019-02-16 17:45:33 +0100

    Ejercicio 2

--- e2_creacion_de_listas/lib/create.ex
+++ e2_creacion_de_listas/lib/create.ex
@@ -1,22 +1,22 @@
 defmodule Create do
 	def create(0),do: [] 
 	def create(n) do
-		create_aux(n,1,[1])
+		create_aux(n,[n])
 	end 
-	def create_aux(m,i,l) when m <= i, do: l
-	def create_aux(m,i,l) when m > i do
-		lista_aux = l ++ [i+1]
-		create_aux(m, i+1, lista_aux)
+	def create_aux(m,l) when m <= 1, do: l
+	def create_aux(m,l) when m > 1 do
+		lista_aux = [m-1|l]
+		create_aux(m-1,lista_aux)
 	end
 
 
 	def reverse_create(0),do: [] 
 	def reverse_create(n) do
-		reverse_create_aux(n,[n])
+		reverse_create_aux(n,1,[1])
 	end 
-	def reverse_create_aux(m,l) when m <= 1, do: l
-	def reverse_create_aux(m,l) when m > 1 do
-		lista_aux = l ++ [m-1]
-		reverse_create_aux(m-1,lista_aux)
+	def reverse_create_aux(m,i,l) when m <= i, do: l
+	def reverse_create_aux(m,i,l) when m > i do
+		lista_aux = [i+1|l]
+		reverse_create_aux(m, i+1, lista_aux)
 	end
 end
